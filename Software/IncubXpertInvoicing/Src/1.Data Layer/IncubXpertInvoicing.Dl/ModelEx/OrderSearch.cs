6941f8227b99aaf6f682c79eb91621fc1c351164
amwabale <a.m.wabale.technical@gmail.com>
2017-10-05 00:00:47 +0530

    added order search

---
+++ Software/IncubXpertInvoicing/Src/1.Data Layer/IncubXpertInvoicing.Dl/ModelEx/OrderSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IncubXpertInvoicing.Dl.ModelEx
+{
+    public class OrderSearch
+    {
+        public string OrderId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string OrderDate { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
